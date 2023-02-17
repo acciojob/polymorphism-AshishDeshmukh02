99d8ffb95523240a5aa846b0b69a471aeba703d8
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-17 12:26:37 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,32 +3,24 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-class Main {
-    public static void main(String[] args) {
-        Product p = new Product();
-
-        int result1 = p.product(2, 3);
-        System.out.println("Result 1: " + result1);
-
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Result 2: " + result2);
-
-        double result3 = p.product(2.0, 3.0);
-        System.out.println("Result 3: " + result3);
-    }
-}
-
 class Product {
-    public int product(int x, int y) {
-        return x * y;
-    }
+  product(x, y) {
+    return x * y;
+  }
 
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
+  product(x, y, z) {
+    return x * y * z;
+  }
 
-    public double product(double x, double y) {
-        return x * y;
-    }
+  product(x, y) {
+    return x * y;
+  }
 }
+
+const p = new Product();
+
+console.log(p.product(2, 3)); // output: 6
+console.log(p.product(2, 3, 4)); // output: 24
+console.log(p.product(2.5, 3.5)); // output: 8.75
+
 
