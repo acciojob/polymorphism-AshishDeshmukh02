dc7a3ab3a72fefd6d4a38f66d85265f199c777bd
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-17 12:27:27 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,24 +3,6 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-class Product {
-  product(x, y) {
-    return x * y;
-  }
 
-  product(x, y, z) {
-    return x * y * z;
-  }
-
-  product(x, y) {
-    return x * y;
-  }
-}
-
-const p = new Product();
-
-console.log(p.product(2, 3)); // output: 6
-console.log(p.product(2, 3, 4)); // output: 24
-console.log(p.product(2.5, 3.5)); // output: 8.75
 
 
